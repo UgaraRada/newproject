9b9b824c726cb21e6b568b59c61be726870adc4e
UgaraRada <d.begaev69@gmail.com>
2020-01-28 22:36:43 +0300

    Add project

---
+++ settings.js
@@ -0,0 +1,90 @@
+Extensions list:
+
+ESLint,
+GitLens,
+Material Theme,
+Cobalt2 Theme Official,
+Path Autocomplete,
+Prettier - Code formatter,
+VSCode-icons,
+Vetur,
+
+{
+    "editor.accessibilitySupport": "off",
+    "editor.cursorBlinking": "smooth",
+    "editor.detectIndentation": false,
+    "editor.fontWeight": "100",
+    "editor.formatOnPaste": false,
+    "editor.formatOnSave": true,
+    "[json]": {
+        "editor.formatOnSave": false
+    },
+    "editor.letterSpacing": 0.3,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.quickSuggestions": {
+        "comments": false,
+        "other": true,
+        "strings": true
+    },
+    "editor.quickSuggestionsDelay": 100,
+    "editor.renderControlCharacters": true,
+    "editor.scrollBeyondLastLine": false,
+    "editor.showFoldingControls": "always",
+    "editor.tabCompletion": "on",
+    "emmet.includeLanguages": {
+        "vue": "html",  
+        "vue-css": "css",
+        "vue-html": "html",
+        "vue-js": "javascript",
+        "vue-scss": "scss",
+        "javascript": "javascriptreact"
+    },
+    "emmet.triggerExpansionOnTab": true,
+    "eslint.alwaysShowStatus": true,
+    "eslint.autoFixOnSave": false,
+    "eslint.validate": [
+        {
+            "autoFix": false,
+            "language": "javascript"
+        },
+        {
+            "autofix": false,
+            "language": "javascriptreact"
+        },
+        {
+            "autoFix": false,
+            "language": "vue"
+        }
+    ],
+    "explorer.confirmDelete": false,
+    "explorer.confirmDragAndDrop": false,
+    "explorer.decorations.colors": false,
+    "explorer.openEditors.visible": 0,
+    "files.insertFinalNewline": true,
+    "prettier.semi": true,
+    "prettier.singleQuote": true,
+    "prettier.trailingComma": "all",
+    "prettier.printWidth": 100,
+    "prettier.tabWidth": 4,
+    "prettier.eslintIntegration": true,
+    "prettier.jsxBracketSameLine": false,
+    "terminal.integrated.shell.windows": "cmd.exe",
+    "terminal.integrated.shellArgs.windows": [
+        "/k",
+        "C:\\Users\\mmolovts\\Programms\\cmder\\vendor\\init.bat" // cmder_root - папка, в которой лежит cmder.exe
+    ],
+    "vetur.format.options.tabSize": 4,
+    "vetur.format.defaultFormatter.html": "js-beautify-html",
+    "vetur.format.defaultFormatterOptions": {
+        "js-beautify-html": {
+            "html.format.wrapAttributes": "force-expand-multiline"
+        },
+        "prettier": {
+            "semi": true,
+            "singleQuote": true,
+            "trailingComma": "all",
+            "printWidth": 140
+        }
+    },
+    "workbench.colorTheme": "Material Theme Palenight High Contrast"
+}
